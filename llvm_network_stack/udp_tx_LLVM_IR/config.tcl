0f7b4224b6989ad89bfb75d8d2e338299ccda0c3
Lanny Lian <lanny2121@gmail.com>
2020-12-09 12:41:38 -0500

    Remove systemc and IR input examples.

--- llvm_network_stack/udp_tx_LLVM_IR/config.tcl
+++
@@ -1,9 +0,0 @@
-source $::env(LEGUP_ROOT_DIR)/examples/legup.tcl
-
-set_project VirtexU9P AWS_F1 hw_only
-
-function_pipeline udpTxReadFunction
-function_pipeline udpTxWriteFunction
-function_pipeline udpTxChecksum
-
-set_custom_top_level_module udpTx
